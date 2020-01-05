29b2449faa53aac10ba8c172f958acae01985eda
Adrian Frith <adrianf@da.org.za>
2020-01-05 22:41:47 +0200

    Hook up map component to query state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, { useCallback } from 'react'
+import { QueryParamProvider, useQueryParams, NumberParam } from 'use-query-params'
 import './App.css'
 import DrawerLayout from './DrawerLayout'
+import Map from './Map'
 
 function App () {
+  console.log('Rendering')
+
+  const [query, setQuery] = useQueryParams({ lat: NumberParam, lon: NumberParam, zoom: NumberParam })
+  const viewport = {
+    center: [query.lat || -28.5, query.lon || 24.6],
+    zoom: query.zoom || 6
+  }
+
+  const handleViewportChanged = useCallback(viewport => {
+    const { center, zoom } = viewport
+    setQuery({
+      lat: center[0].toFixed(5), // see https://xkcd.com/2170/
+      lon: center[1].toFixed(5),
+      zoom
+    })
+  }, [setQuery])
+
   return (
-    <DrawerLayout
-      title='Dot Map of South Africa'
-      mainContent={<div style={{ backgroundColor: 'red', height: '100%' }}>Map</div>}
-      drawerTitle='Settings'
-      drawerContent='Controls'
-    />
+    <QueryParamProvider history={window.history}>
+      <DrawerLayout
+        title='Dot Map of South Africa'
+        mainContent={<Map viewport={viewport} onViewportChanged={handleViewportChanged} />}
+        drawerContent='Controls'
+      />
+    </QueryParamProvider>
   )
 }
 
